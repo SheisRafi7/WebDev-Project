c57b9395f56582bd733065a8f1cacea1afadce61
SheisRafi7 <146082463+SheisRafi7@users.noreply.github.com>
2024-04-29 01:59:25 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container">
+        <p>&copy;<?php echo date("2024"); ?> Designers Den. All rights reserved.</p>
+    </div>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Designer's Den</title>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<header>
+    <div class="container">
+        <h1>Designers Den</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="products.php">Products</a></li>
+                <li><a href="reviews">Review</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
